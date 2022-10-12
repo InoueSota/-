22c5d0e65950494d13e8246d7bd6db4013663991
k022g0099@g.neec.ac.jp <k022g0099@g.neec.ac.jp>
2022-10-12 12:05:44 +0900

    ステージ内に生成

--- Figure.h
+++ Figure.h
@@ -18,6 +18,7 @@ public:
 	virtual void draw(){};
 	virtual void set() {};
 	virtual void respon() {};
+	virtual bool IsInStage() { return 0; };
 	bool cheakdraw(Player player, Vec2 Position, bool Flag);
 	//画面内かチェック
 	bool InScreen(Player player, Vec2 Position);
@@ -26,6 +27,8 @@ public:
 	float radian;
 	unsigned int color;
 	bool flag;
+	//消
+	float stage = 10000;
 private:
 	
 };
@@ -37,6 +40,7 @@ public:
 	void draw(Screen& screen);
 	void set(Player& player);
 	void respon(Player player);
+	bool IsInStage(float stage);
 private:
 
 };
@@ -48,6 +52,7 @@ public:
 	void draw(Screen& screen);
 	void set(Player& player);
 	void respon(Player player);
+	bool IsInStage(float stage);
 
 	Vec2 top_position;
 	Vec2 right_position;
@@ -63,6 +68,7 @@ public:
 	void draw(Screen& screen);
 	void set(Player& player);
 	void respon(Player player);
+	bool IsInStage(float stage);
 
 	Vec2 top_left_position;
 	Vec2 top_right_position;
--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
-	//SRAND();
+	
 
 	for (int i = 0; i < Figure::FigureMax; i++) {
 		ellipse[i].set(players);
@@ -125,8 +125,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		
 		}
 		players.Draw(screen,players);
-
-
 		///
 		/// ↑描画処理ここまで
 		///
